53a37039bd1985fed8bc437afebb3d6655ae81aa
Unknown <qkfrlgy@naver.com>
2018-06-27 20:22:07 +0900

    연산자예제

--- src/day1/heollworld.java
+++ src/day1/heollworld.java
@@ -2,8 +2,13 @@ package day1;
 
 public class heollworld {
 /*
+ * public[공공의] : 접근제한자[누구나접근가능]
+ * class : 클래스임을 나타냄
  * static : 클래스를 통해 호출함
  * 자바에서 프로그램을 싱행하려면 main메소드가 있어야한다.[중요]
+ * void : 반환이 없음, 메소드설명에서 자세히
+ * mine : 메소드 이름
+ * (String[] args) = 매개변수
  *  */
 	public static void main(String[] args) {
 		//표준출력메소드= ststem.out.println
