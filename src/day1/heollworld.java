2f212f915b3262d55636c79428ade5abdbef7a52
Unknown <qkfrlgy@naver.com>
2018-06-27 16:28:28 +0900

    1화차 : he;;oworld

---
+++ src/day1/heollworld.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class heollworld {
+/*
+ * 자바에서 프로그램을 싱행하려면 main메소드가 있어야한다.[중요]
+ *  */
+	public static void main(String[] args) {
+		//표준출력메소드
+System.out.println("helloworld");
+System.out.print("helloworld");
+	}
+
+}
