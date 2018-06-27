4c9f5eed2f9edc5a44c8945c67c854aaf0de7aff
Unknown <qkfrlgy@naver.com>
2018-06-27 17:25:11 +0900

    1

--- src/day1/heollworld.java
+++ src/day1/heollworld.java
@@ -2,10 +2,11 @@ package day1;
 
 public class heollworld {
 /*
+ * static : 클래스를 통해 호출함
  * 자바에서 프로그램을 싱행하려면 main메소드가 있어야한다.[중요]
  *  */
 	public static void main(String[] args) {
-		//표준출력메소드
+		//표준출력메소드= ststem.out.println
 System.out.println("helloworld");
 System.out.print("helloworld");
 	}
---
+++ src/day1/variable.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class variable {
+	public static void main(String[] args) {
+	//변수규칙
+		//int 1num; //변수명 처음에는 숫자가 올 수 없음
+	 //int num%; //특수문자는 $_ aks rksmd
+	// int int; // 키워드사용불가
+	 //int num 1; //공백사용불가
+	 int num;
+	 //int num; // 동일한 변수는 선언 불가
+	 int Num; // 대소문자 구별하여 다른 변수로 취급
+	 //변수관례
+	 int studentScore; // 두단어 이상으로 변수명을 만들경우 두번제 당어의 첫글자를 대문자로 한다 : 카멜[낙타]표기법
+	 int a; // [x] 변수는 변수명을 보고 해당하는 역할을 알 수 있어야 한다.
+	 num = 10; //항상 세미콜론 ; 으로 마무리해야함 10 = num [x]
+	 System.out.println(num);
+	}
+
+}
